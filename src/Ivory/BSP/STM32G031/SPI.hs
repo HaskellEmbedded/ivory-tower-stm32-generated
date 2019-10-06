80c1b1fd5ad688f20ca4e82712c6f2c38388f4cc
Richard Marko <srk@48.io>
2019-10-06 20:17:29 +0200

    G0/G4 SPIs

---
+++ src/Ivory/BSP/STM32G031/SPI.hs
@@ -0,0 +1,35 @@
+module Ivory.BSP.STM32G031.SPI (
+    spi1
+  , spi2
+  ) where
+
+
+import Ivory.Language
+import Ivory.HW
+
+import Ivory.BSP.STM32.ClockConfig
+
+import Ivory.BSP.STM32G031.RCC
+import Ivory.BSP.STM32G031.MemoryMap
+import qualified Ivory.BSP.STM32G031.Interrupt as G031
+
+import Ivory.BSP.STM32.Peripheral.SPI
+
+spi1 :: SPI
+spi1 = mkSPI spi1_periph_base
+          rccenable rccdisable
+          G031.SPI1
+          PClk1 "spi1"
+  where
+  rccenable  = modifyReg rcc_reg_apbenr2 $ setBit   rcc_apbenr2_spi1en
+  rccdisable = modifyReg rcc_reg_apbenr2 $ clearBit rcc_apbenr2_spi1en
+
+spi2 :: SPI
+spi2 = mkSPI spi2_periph_base
+          rccenable rccdisable
+          G031.SPI2
+          PClk1 "spi2"
+  where
+  rccenable  = modifyReg rcc_reg_apbenr1 $ setBit   rcc_apbenr1_spi2en
+  rccdisable = modifyReg rcc_reg_apbenr1 $ clearBit rcc_apbenr1_spi2en
+
---
+++ src/Ivory/BSP/STM32G474/SPI.hs
@@ -0,0 +1,45 @@
+module Ivory.BSP.STM32G474.SPI (
+    spi1
+  , spi2
+  , spi4
+  ) where
+
+
+import Ivory.Language
+import Ivory.HW
+
+import Ivory.BSP.STM32.ClockConfig
+
+import Ivory.BSP.STM32G474.RCC
+import Ivory.BSP.STM32G474.MemoryMap
+import qualified Ivory.BSP.STM32G474.Interrupt as G474
+
+import Ivory.BSP.STM32.Peripheral.SPI
+
+spi1 :: SPI
+spi1 = mkSPI spi1_periph_base
+          rccenable rccdisable
+          G474.SPI1
+          PClk1 "spi1"
+  where
+  rccenable  = modifyReg rcc_reg_apb2enr $ setBit   rcc_apb2enr_spi1en
+  rccdisable = modifyReg rcc_reg_apb2enr $ clearBit rcc_apb2enr_spi1en
+
+spi2 :: SPI
+spi2 = mkSPI spi2_periph_base
+          rccenable rccdisable
+          G474.SPI2
+          PClk1 "spi2"
+  where
+  rccenable  = modifyReg rcc_reg_apb1enr1 $ setBit   rcc_apb1enr1_spi2en
+  rccdisable = modifyReg rcc_reg_apb1enr1 $ clearBit rcc_apb1enr1_spi2en
+
+spi4 :: SPI
+spi4 = mkSPI spi4_periph_base
+          rccenable rccdisable
+          G474.SPI4
+          PClk1 "spi4"
+  where
+  rccenable  = modifyReg rcc_reg_apb2enr $ setBit   rcc_apb2enr_spi4en
+  rccdisable = modifyReg rcc_reg_apb2enr $ clearBit rcc_apb2enr_spi4en
+
